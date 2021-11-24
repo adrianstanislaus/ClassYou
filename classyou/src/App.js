5a52299b7cd0a2911c2467e4fae7700a9c43ab56
adrianstanislaus <18118036@telecom.stei.itb.ac.id>
2021-11-24 15:20:48 +0700

    add header

--- classyou/src/App.js
+++ classyou/src/App.js
@@ -1,15 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p class="text-gray-500 font-bold">
-          App ClassYou dalam Pembangunan
-        </p>
+    <div>
+      <header className="border bg-yellow-300 ">
+        <navbar className="h-10 container mx-auto mt-4 grid grid-cols-6 gap-3 border">
+          <logo className="h-10 col-span-1 border ">
+            <p className="border">ClassYou</p>
+          </logo>
+          <menu className="border flex m-0 col-span-4 ">
+            <search className="border">
+              <input type="text"/>
+            </search>
+            <ul className="flex border">
+              <li className="border"><a>Explore</a></li>
+              <li className="border"><a>About</a></li>
+            </ul>
+          </menu>
+          <user className="h-10 col-span-1 border">
+          <p className="border">Admin</p>
+          </user>
+        </navbar>
       </header>
+      
     </div>
   );
 }
